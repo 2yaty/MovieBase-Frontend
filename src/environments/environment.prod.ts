314726d28f161f936dcc4810f0750b4b326cfc6c
Mohamed Ali Qiaty <eng.ayaty@gmail.com>
2025-02-24 02:53:30 +0200

    fix: update production API URL and set production flag to true in environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.moviebase.com' // Replace with your production API URL
+  apiUrl: 'https://movie-base-1b8b37b4b354.herokuapp.com/' // Replace with your production API URL
 };
